7e881ae699b48cad2a5416ddd9a6f4a7b521ac18
Sara Fotros <sarafotros1@yahoo.com>
2019-11-13 11:04:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,7 +22,8 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   literal_aoh =[
-    {
+    { name:  "Don Gately", occupation: "Live-in Staff" },
+    
     ]
 end
 
